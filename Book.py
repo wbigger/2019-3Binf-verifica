9865ce90eb2957a6eebde14a0e732a45becf6704
RedMosca98 <RedMosca98@gmail.com>
2019-10-29 09:25:47 +0100

    passed bulky tests and valuable tests

--- Book.py
+++ Book.py
@@ -6,3 +6,9 @@ class Book:
         self.year = year
         self.pages = pages
         self.isbn = isbn
+
+        self.is_bulky = (self.pages > 500)
+
+    def is_valuable(self, current_year):
+        age = (current_year - self.year)
+        return age < 1 or age > 20
