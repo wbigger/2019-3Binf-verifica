abd36cccc14e5084cd6cc0e34fc66de2edab592e
Your Name <you@example.com>
2019-10-29 15:29:40 +0600

    consegna

--- Locker.py
+++ Locker.py
@@ -1,3 +1,17 @@
 class Locker:
-    def __init__(self, position,b,c,d,e,f):
+    def __init__(self,position,width,height,depth,material,private_key):
         self.position = position
+        self.width = width 
+        self.height = height
+        self.depth = depth
+        self.material = material 
+        self.private_key = private_key
+        if width*height*depth > 2000000:
+            self.is_bulky = true
+            else:
+                self.is_bulky = false
+
+
+
+
+
