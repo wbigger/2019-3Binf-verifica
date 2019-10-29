8c6bfac8b7832339e7fd5898ef0027dce9eb02c9
RedMosca98 <RedMosca98@gmail.com>
2019-10-29 09:52:05 +0100

    passed all test

--- Book.py
+++ Book.py
@@ -12,3 +12,23 @@ class Book:
     def is_valuable(self, current_year):
         age = (current_year - self.year)
         return age < 1 or age > 20
+
+    def is_isbn_valid(self):
+        check_digit = self.isbn % 10
+
+        digits = []
+
+        digits = list(str(self.isbn))
+        digits.pop()
+        digits = list(map(int, digits))
+
+        expected_check_digit = 0
+
+        for index in range(len(digits)):
+            expected_check_digit += digits[index] * (index + 1)
+
+        expected_check_digit %= 11
+
+        print(expected_check_digit)
+
+        return expected_check_digit == check_digit
