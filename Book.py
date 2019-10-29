ab38f1532766241945872795eb2d36fd3bd82f40
RedMosca98 <RedMosca98@gmail.com>
2019-10-29 09:17:38 +0100

    removed line B

--- Book.py
+++ Book.py
@@ -1,3 +1,3 @@
 class Book:
-    def __init__(self,title,b,c,d,e,f):
+    def __init__(self, title, b, c, d, e, f):
         self.title = title
--- Locker.py
+++
@@ -1,3 +0,0 @@
-class Locker:
-    def __init__(self, position,b,c,d,e,f):
-        self.position = position
