db0b9baacfe0d72634e419bda9f70e96a9989f5b
Your Name <you@example.com>
2019-10-29 10:27:13 +0100

    Consegna compito

--- Book.py
+++ Book.py
@@ -1,3 +1,22 @@
 class Book:
-    def __init__(self,title,b,c,d,e,f):
+    def __init__(self,title,author,publisher,year,pages,isbn):
         self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.year = year 
+        self.pages = pages
+        self.isbn = isbn
+        self.is_bulky = self.pages > 500
+    
+    def is_valuable(self,this_book):
+        if this_book > 1997 and this_book < 2017:
+            return True
+        else:
+            return False
+
+            
+    
+
+
+
+
