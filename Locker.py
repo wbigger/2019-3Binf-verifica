f5ca89ef9b48028d246c620fc6fa12efa65ffccd
Your Name <you@example.com>
2019-10-29 15:34:20 +0600

    consegna

--- Locker.py
+++ Locker.py
@@ -7,11 +7,14 @@ class Locker:
         self.material = material 
         self.private_key = private_key
         if width*height*depth > 2000000:
-            self.is_bulky = true
-            else:
-                self.is_bulky = false
-
-
+            self.is_bulky = True
+        else:
+            self.is_bulky = False
+    def is_code_valid(self,code) :
+        if len(code)>=4 and len(code) <=8:
+        return True
+    else:
+        return False
 
 
 
