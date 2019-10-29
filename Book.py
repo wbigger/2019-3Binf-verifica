c85150b71c915c713336bc5b1d3e965f126b8c3b
Gabriele Cerqua <gab.quercia@gmail.com>
2019-10-29 10:31:26 +0100

    consegna_compito

--- Book.py
+++ Book.py
@@ -1,3 +1,15 @@
 class Book:
-    def __init__(self,title,b,c,d,e,f):
+    def __init__(self,title,author,publisher,year,pages,isbn):
         self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.year = year
+        self.pages = pages
+        self.isbn = isbn
+        self.is_bulky = self.pages >= 500
+
+    def is_valuable(self,year):
+            if 2019 - self.year >= 20 or 2019-self.year <= 1:
+                return True
+            else:
+               return False
