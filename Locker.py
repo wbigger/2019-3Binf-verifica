5c964e7d0119d391a747e50515d30b9a414a657f
Rick <riccardoforteleoni@icloud.com>
2019-10-29 10:27:11 +0100

    consegna compito

--- Locker.py
+++ Locker.py
@@ -1,3 +1,15 @@
 class Locker:
-    def __init__(self, position,b,c,d,e,f):
+    def __init__(self, position,width,height,depth,material,private_key):
         self.position = position
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.material = material
+        self.private_key = private_key
+        self.is_bulky = (self.width * self.height * self.depth) > 2000000
+        self.is_bulky = (160 * 180 * 50) > 2000000
+        self.is_bulky = (self.width * self.height * self.depth) > 2000000
+        
+    
+  
+    
