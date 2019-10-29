f00922698dd3254adc324573184f800bf4db9991
ibarboxxx <gabriel.tintori2@gmail.com>
2019-10-29 10:27:37 +0100

    Consegna compito

--- Locker.py
+++ Locker.py
@@ -1,3 +1,20 @@
 class Locker:
-    def __init__(self, position,b,c,d,e,f):
+    def __init__(self, position,width,height,depth,material,private_key):
         self.position = position
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.material = material
+        self.private_key = private_key 
+        self.volume = height* width *depth
+        if self.volume > 2000000:
+            self.is_bulky = True
+        else:
+            self.is_bulky = False
+        def is_code_valid()
+
+
+     
+
+   
+
