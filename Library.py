9ff613ce05733b0d5c94ddec8d3cffaec6342501
wbigger <wbigger@gmail.com>
2019-11-05 10:36:39 +0100

    Add library class

---
+++ Library.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from Book import Book
+
+class Library:
+    def __init__(self, name, code):
+        self.name = name
+        self.sbn_code = code
+        self.last_update = datetime.now()
+        self.catalogue = []
+    
+    def add_book(self,book):
+        self.catalogue.append(book)
+        self.last_update = datetime.now()
+
+marconi = Library("Biblioteca IIS Marconi","CIVMA")
+
+harry_potter = Book("Harry Potter qualcosa","JKR","Salani",1997,350,12345)
+
+marconi.add_book(harry_potter)
+
+print(marconi.name)
+print(marconi.sbn_code)
+print(marconi.last_update)
+print(marconi.catalogue)
+
+
