a5e2ff331f47230217d17241e00b0191f7a1d54f
Your Name <you@example.com>
2019-11-05 10:36:08 +0100

    add Library

--- Book.py
+++ Book.py
@@ -1,21 +1,24 @@
 class Book:
-    def __init__(self,title,author,publisher,year,pages,isbn):
+    def __init__(self,title,author,publisher,year, pages, isbn):
         self.title = title
         self.author = author
         self.publisher = publisher
-        self.year = year 
+        self.year = year
         self.pages = pages
         self.isbn = isbn
         self.is_bulky = self.pages > 500
+        
+    def is_valuable(self,current_year):
+        return current_year - self.year > 20 or current_year - self.year < 1
     
-    def is_valuable(self,this_book):
-        if this_book > 1997 and this_book < 2017:
-            return True
-        else:
-            return False
-
-            
-    
+    def is_isbn_valid(self):
+        idx = 1
+        accumulator = 0
+        for i in str(self.isbn)[:-1]:
+            accumulator += int(i) * idx
+            idx +=1
+        accumulator = accumulator % 11
+        return accumulator == int(str(self.isbn)[-1:])
 
 
 
