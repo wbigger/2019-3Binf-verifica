d3fdd1bbb2e37bf4072346f2515d38aabc71775b
Your Name <you@example.com>
2019-11-05 10:35:18 +0100

    Library added

--- Book.py
+++ Book.py
@@ -1,30 +1,21 @@
 class Book:
-    def __init__(self, title, author, publisher, year, pages, isbn):
+    def __init__(self,title,author,publisher,year, pages, isbn):
         self.title = title
         self.author = author
         self.publisher = publisher
         self.year = year
         self.pages = pages
         self.isbn = isbn
-
-        self.is_bulky = (self.pages > 500)
-
-    def is_valuable(self, current_year):
-        age = (current_year - self.year)
-        return age < 1 or age > 20
-
+        self.is_bulky = self.pages > 500
+        
+    def is_valuable(self,current_year):
+        return current_year - self.year > 20 or current_year - self.year < 1
+    
     def is_isbn_valid(self):
-        check_digit = self.isbn % 10
-
-        digits = list(str(self.isbn))
-        digits.pop()
-        digits = list(map(int, digits))
-
-        expected_check_digit = 0
-
-        for index in range(len(digits)):
-            expected_check_digit += digits[index] * (index + 1)
-
-        expected_check_digit %= 11
-
-        return expected_check_digit == check_digit
+        idx = 1
+        accumulator = 0
+        for i in str(self.isbn)[:-1]:
+            accumulator += int(i) * idx
+            idx +=1
+        accumulator = accumulator % 11
+        return accumulator == int(str(self.isbn)[-1:])
