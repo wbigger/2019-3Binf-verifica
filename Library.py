36c2d34d2623f1490688650afa59c12f811e33d6
Your Name <you@example.com>
2019-11-09 09:44:46 +0100

    add test from library

--- Library.py
+++ Library.py
@@ -3,20 +3,11 @@ from Book import Book
 class Library:
     def __init__(self,name,code):
         self.name = name
-        self.code = code
+        self.sbn_code = code
         self.last_update = datetime.now()
         self.catalogue = []
 
-def add_book(self,book):
-    self.catalogue.append(book)
 
-marconi = Library("Biblioteca IIS Marconi","CIVMA")
+    def add_book(self,book):
+        self.catalogue.append(book)
 
-harry.potter = Book("Harry Potter qualcosa","JKR","Salani",1997,350,12345)
-
-marconi.add_book(harry_potter)
-
-print(marconi.name)
-print(marconi.code)
-print(marconi.last_update)
-print(marconi.catalogue)
