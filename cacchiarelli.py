92ea41855d4ae2e652fc471eacda765dca5a2e4a
Error404MySelfNotFound <56788696+Error404MySelfNotFound@users.noreply.github.com>
2019-11-23 10:43:12 +0200

    Create cacchiarelli.py

---
+++ cacchiarelli.py
@@ -0,0 +1,8 @@
+from Book import Book
+
+il_capitan_fracassa = Book("il capitan fracassa","thèophile gautier","",2010,481,"IT\\ICCU\\ANA\\0343102")
+le_avventure_di_oliver_twist = Book("le avventure di oliver twist","charles dickens","",2010,"IT\\ICCU\\ANA\\0328890")
+
+
+
+
