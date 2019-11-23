ccf3730742b306c492ea4e284bd0438027f5a5b6
Your Name <you@example.com>
2019-11-23 08:34:59 +0200

    cerqua

---
+++ cerqua.py
@@ -0,0 +1,7 @@
+from Book import Book
+
+
+book_1 = Book("i fratelli karamazov", "fedor dostoevskij", "Rizzoli", 2010, 475, "IT\ICCU\MIL\0802714")
+
+books = [book_1]
+print(books[0].title,",", books[0].author)
