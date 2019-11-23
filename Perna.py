d2ce939497c9705db4b4cc2cb8a7f0e5ec07d423
No name <none>
2019-11-23 12:44:03 +0400

    aggiunta libri

---
+++ Perna.py
@@ -0,0 +1,13 @@
+from Book import Book
+
+i_fratelli_karamazov = Book("I Fratelli Karamazov", "Fedor Dostoevskij", "Rizzoli", 2010, 374, "IT\\ICCU\\IEI\\0472625")
+il_rosso_e_il_nero = Book("Il rosso e il nero", "Stendhal", "Rizzoli", 2010, 388, "IT\\ICCU\\MOD\\0738722")
+
+books = [i_fratelli_karamazov, il_rosso_e_il_nero]
+
+assert book[0] == i_fratelli_karamazov
+assert book[1] == il_rosso_e_il_nero
+
+def get_books_list(the_list):
+    return books
+    
