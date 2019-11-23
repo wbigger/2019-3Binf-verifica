9fa796a23b938e6ee6265912b850c73cf5ff8459
Your Name <you@example.com>
2019-11-23 09:38:06 +0100

    aggiunta libri per database

--- Library.py
+++ Library.py
@@ -10,15 +10,4 @@ class Library:
 
     def add_book(self,Book):
         self.catalogue.append(Book)
-
-marconi = Library("BIBLIOTECA IIS Marconi", "CIVMA")
-harry_potter = Book("Harry Potter e la pietra filosofale","J. K. Rowling","Salani Editore",1997, 294, 8877827025)
-
-assert marconi.name == "BIBLIOTECA IIS Marconi"
-assert marconi.sbn_code == "8877827025"
-assert marconi.last_update == ""
-assert len(marconi.catalogue) == "0"
-
-marconi.add_book(harry_potter)
-
-assert len(marconi.catalogue) == "1"
+        
