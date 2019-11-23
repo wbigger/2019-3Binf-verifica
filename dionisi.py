9931cdf009f9a2a9cf15cdba30ad3f6b4105445e
wbigger <wbigger@gmail.com>
2019-11-23 12:56:36 +0100

    Get Database

---
+++ dionisi.py
@@ -0,0 +1,12 @@
+from Book import Book
+
+Moll_Flanders = Book("Moll Flanders","Daniel Defoe","Rizzoli",2010,402,"IT\\ICCU\\MIL\\0802697")
+le_affinità_elettive = Book("le affinità elettive","Johann Wolfgang Goethe","Rizzoli",2010,286,"IT\\ICCU\\ANA\\0341464")
+
+books = [Moll_Flanders, le_affinità_elettive]
+
+def get_books_list():
+    return books
+
+
+
