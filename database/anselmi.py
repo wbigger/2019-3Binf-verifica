6a892a3f61548a7e6463f05442a3ee3474a0b93b
wbigger <wbigger@gmail.com>
2019-11-25 15:46:38 +0100

    Remove debug lines

--- database/anselmi.py
+++ database/anselmi.py
@@ -4,11 +4,6 @@ martin_eden = Book("Martin_Eden","Jack_London","Rizzoli",2010,460,"IT\\ICCU\\TO0
 ritratto_di_signora_II = Book("Henry_James","Ritratto_di_Signora_II","",2010,355,"IT\\ICCU\\ANA\\0343680")
 
 books = [martin_eden,ritratto_di_signora_II]
-print(books)
-print(martin_eden)
-books[0].title = "Scheerzetto"
-
-assert martin_eden.title == "Scheerzetto"
 
 def get_books_list():
     return books
