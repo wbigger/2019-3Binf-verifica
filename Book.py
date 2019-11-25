0bf44ce0c42464dd06498f9cfca0c13103760725
Ionut Cicio <cicioionut328@gmail.com>
2019-11-25 16:36:12 +0100

    updated book

--- Book.py
+++ Book.py
@@ -1,21 +1,42 @@
 class Book:
-    def __init__(self,title,author,publisher,year, pages, isbn):
+    def __init__(self,title,author,publisher,year, pages, book_id):
         self.title = title
         self.author = author
         self.publisher = publisher
         self.year = year
         self.pages = pages
-        self.isbn = isbn
+        self.isbn = book_id if self.is_isbn_valid(book_id) else None
+        self.sbn_code = book_id if self.is_sbn_code_valid(book_id) else None
         self.is_bulky = self.pages > 500
         
     def is_valuable(self,current_year):
         return current_year - self.year > 20 or current_year - self.year < 1
     
-    def is_isbn_valid(self):
+    def is_isbn_valid(self,isbn):
         idx = 1
         accumulator = 0
-        for i in str(self.isbn)[:-1]:
+        isbn_base = str(isbn)[:-1]
+
+        if isbn_base.isdigit() == False:
+            return False
+
+        isbn_check = int(str(isbn)[-1:])
+
+        for i in isbn_base:
             accumulator += int(i) * idx
             idx +=1
         accumulator = accumulator % 11
-        return accumulator == int(str(self.isbn)[-1:])
+        print(isbn_check)
+        print(accumulator)
+        print(accumulator == isbn_check)
+        return accumulator == isbn_check
+
+    def is_sbn_code_valid(self,code):
+        # TODO: add a validation through a regular expression
+        return True
+
+    def has_isbn(self):
+        return self.isbn != None
+
+    def has_sbn_code(self):
+        return self.sbn_code != None
