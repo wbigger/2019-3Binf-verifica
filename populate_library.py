d1f72837ffcc68e737552edd662dc9153b66c321
Claudio Capobianco <claudio.capobianco@istruzione.it>
2019-11-26 10:33:10 +0100

    Add JSON support

--- populate_library.py
+++ populate_library.py
@@ -1,3 +1,5 @@
+import json
+
 from Library import Library
 
 from database.anselmi import get_books_list as get_anselmi # error in book order (risolto)
@@ -86,12 +88,5 @@ marconi.add_books(get_verticelli())
 expected_catalogue_len += 2
 assert len(marconi.catalogue) == expected_catalogue_len, f"Expected {expected_catalogue_len}, got {len(marconi.catalogue)}"
 
-# a = "ciao"
-# b = bin(7)
-# c = True.bit_length()
-# d = 7.0.fromhex("12")
-# print("{} is {}".format(type(a),hex(id(a))))
-# print("{} is {}".format(b,hex(id(b))))
-# print("{} is {}".format(c,hex(id(c))))
-# print("{} is {}".format(d,hex(id(d))))
-# print("{} is {}".format(marconi.catalogue,hex(id(marconi.catalogue))))
+for b in marconi.catalogue:
+    print(json.dumps(b.__dict__))
