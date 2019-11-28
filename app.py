eefbe7ce15ae2aa33ab9da1979390a70cf3ee9c2
wbigger <wbigger@gmail.com>
2019-11-28 13:31:44 +0100

    Add comments to json.dumps()

--- app.py
+++ app.py
@@ -5,4 +5,10 @@ app = Flask("marconi")
 
 @app.route("/")
 def data_book():
-    return json.dumps([b.__dict__ for b in full_catalogue])
+    return json.dumps(
+        
+        # create a list with all the books in the catalogue
+        # book.__dict__ create a json with all the variables of the class Book
+        [book.__dict__ for book in full_catalogue]
+        
+        )
